091eb71577e7b9b2ad2ecf9dc9461bccb9ad8ffe
Tom Nielsen <tomn@tomn-desktop.(none)>
2010-03-31 16:19:10 +0100

    xmonad and cabal config

---
+++ xmonad.hs
@@ -0,0 +1,11 @@
+import XMonad
+import XMonad.Config.Gnome
+
+main = do
+  xmonad $ gnomeConfig
+    { terminal    = "gnome-terminal"
+    , modMask     = mod4Mask
+    , focusFollowsMouse = False
+    , borderWidth = 2
+    }
+
